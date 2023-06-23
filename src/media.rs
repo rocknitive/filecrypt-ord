3fac5838d3b892115deda07e8ded96780cefccc7
akrckn <alexander@rocknitive.com>
2023-06-23 19:00:18 +0200

    changed to be able to upload files with enc ending

--- src/media.rs
+++ src/media.rs
@@ -21,7 +21,7 @@ impl Media {
     ("application/pdf", Media::Pdf, &["pdf"]),
     ("application/pgp-signature", Media::Text, &["asc"]),
     ("application/yaml", Media::Text, &["yaml", "yml"]),
-    ("application/zlib", Media::Text, &["zlib"]),
+    ("application/enc", Media::Text, &["enc"]),
     ("audio/flac", Media::Audio, &["flac"]),
     ("audio/mpeg", Media::Audio, &["mp3"]),
     ("audio/wav", Media::Audio, &["wav"]),
